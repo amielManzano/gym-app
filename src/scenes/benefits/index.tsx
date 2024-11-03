e940f35b74e45b60b624991986064ac43433d0f9
aemanzano <manzano.amiel@nexseed.net>
2024-11-04 00:37:27 +0800

    removed unused import

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from '@heroicons/react/24/solid'
 import { BenefitType, SelectedPage } from '@/assets/shared/types'
 import { motion } from 'framer-motion'
